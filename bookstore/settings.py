e901d1b0c4bd4f5545d3f4db96bac1469aed5df9
fernando <fernando100@windowslive.com>
2025-01-06 20:00:34 -0300

    proc

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -142,7 +142,6 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-
 # REST Framework settings
 REST_FRAMEWORK = {
 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
